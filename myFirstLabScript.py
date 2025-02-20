e15d0b4298085a5a9ca7cc94e7d9fdd0056fb19c
BarisDursun <154085149+BarisDursun@users.noreply.github.com>
2025-02-20 15:16:04 +0300

    Create myFirstLabScript.py
    Python

---
+++ myFirstLabScript.py
@@ -0,0 +1,30 @@
+x=input('What is your name?')
+print('Hello'+x)
+y=input('What is your Student ID?')
+print('Your ID is'+y)
+
+var1=int(input('enter variable='))
+var2=int(input('enter variable='))
+print(var1+var2)
+print(var1-var2)
+print(var1*var2)
+
+name=input('what is your name?')
+gradeLab=int(input('lab grade='))
+gradeMidterm=int (input('Midterm Grade='))
+gradeFinal=int (input('final grade='))
+print('Last score=',((gradeLab* 0.25)+(gradeMidterm*0.35)+(gradeFinal*0.40)))
+
+print('*')
+print('**')
+print('***')
+print('**')
+print('*')
+
+
+
+
+
+
+
+
